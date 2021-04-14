7227d4e057f0e5f417c19bc2a8f91d60ad3b6ca8
Nyan Wunna <nyanwunna@my.jcu.edu.au>
2021-04-14 23:14:40 +0630

    add username prompt

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -11,3 +11,10 @@ print("The last number is {}".format(numbers[-1]))
 print("The smallest number is {}".format(min_number))
 print("The largest number is {}".format(max_number))
 print("The average of the numbers is {}".format(average_number))
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Enter username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
