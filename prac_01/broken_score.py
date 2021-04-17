439375d689883121f0f6f2c8b3648c56b4063491
Nyan Wunna <nyanwunna@my.jcu.edu.au>
2021-04-17 21:42:30 +0630

    reformat file

--- prac_01/broken_score.py
+++ prac_01/broken_score.py
@@ -3,13 +3,20 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-else:
-    if 90 > score > 50:
-        print("Passable")
-    elif score >= 90:
-        print("Excellent")
+
+def main():
+
+
+    score = float(input("Enter score: "))
+    if score < 0 or score > 100:
+        print("Invalid score")
     else:
-        print("Bad")
+        if 90 > score > 50:
+            print("Passable")
+        elif score >= 90:
+            print("Excellent")
+        else:
+            print("Bad")
+
+
+main()
